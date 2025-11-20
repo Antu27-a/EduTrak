7c92187d8ce295b3f5968c64c40df35b994afd64
Antu27-a <superantugoku@gmail.com>
2025-11-20 15:46:03 -0300

    nose

--- Server/app.js
+++ Server/app.js
@@ -1,7 +1,9 @@
 const Express=require('express')
 const App=Express()
 
-const PORT=5000
+require('dotenv').config()
+
+const PORT=process.env.PORT||5000
 
 App.listen(PORT,()=>{
     console.log(`🚀http://localhost: ${PORT}`)
